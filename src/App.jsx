e7f9ad18d47944e47a906e86fd1862df9d0b1ef2
SutPi0154 <sutpi070@gmail.com>
2023-03-10 07:33:53 +0630

    ready now

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./components/Home";
+import Home from "./Pages/Home";
+import Navbar from "./components/Navbar";
+// import { Home } from "./components/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Home = () => {
-  return <div>Home
-    <h1>Min ga lar par</h1>
-  </div>;
-};
